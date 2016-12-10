1308b16821398a3cfd49f88c560db15fe8fe1c69
hello <bonner_bo@yahoo.com>
2016-12-10 01:10:27 +0000

    added files and folders and some connections

---
+++ league/app_api/models/championData.js
@@ -0,0 +1,25 @@
+var mongoose = require('mongoose');
+
+var championschema = new mongoose.Schema({
+    name: String,
+    description: String,
+    passivename: String,
+    passivedesc: String,
+    abilityonename: String,
+    abilityonedesc: String,
+    abilitytwoname: String,
+    abilitytwodesc: String,
+    abilitythreename: String,
+    abilitythreedesc: String,
+    abilityfourname: String,
+    abilityfourdesc: String
+    
+});
+
+var summoner = new mongoose.Schema({ 
+    Name: String,
+    Losses: Number, 
+    Wins: Number
+});
+
+mongoose.model('Champions', championschema);
